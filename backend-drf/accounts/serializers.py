ea5014a8d58a1902982261ba06fb346065901f17
celcius001 <itscelciusgaming@gmail.com>
2026-01-16 14:21:40 +0800

    adjusted

--- backend-drf/accounts/serializers.py
+++ backend-drf/accounts/serializers.py
@@ -1,16 +1,17 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8, style={'input_type': 'password'})
+    password = serializers.CharField(
+        write_only=True, min_length=8, style={"input_type": "password"}
+    )
+
     class Meta:
         model = User
-        fields = [ 'username', 'email', 'password' ]
-    
+        fields = ["username", "email", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
     def create(self, validated_data):
-        user = User.objects.create_user(
-            validated_data['username'],
-            validated_data['email'],
-            validated_data['password']
-        )
+        user = User.objects.create_user(**validated_data)
         return user
--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
-from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
